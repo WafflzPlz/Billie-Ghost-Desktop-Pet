a5cad483ea28638cc5545dfc2318df99cbfe1d85
WafflzPlz <michael.kogler1@gmx.at>
2023-12-18 16:24:50 +0100

    no real changes, just tried stuff out

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ PIC_PATH = os.getenv("PIC_PATH")
 def main():
     root = tk.Tk()
     root.geometry("128x128")
+    #root.config(bg="#00008b")
+    #root.wm_attributes("-transparentcolor", "#0000b8")
+    root.overrideredirect(True)
+
 
     img = Image.open(os.path.join(PIC_PATH, "idle.png"))
     img = img.resize((128, 128), Image.NEAREST)
@@ -17,6 +21,9 @@ def main():
 
     label = tk.Label(image=img)
     label.pack()
+
+    #root.wait_visibility(root)
+    #root.wm_attributes("-alpha", 0.5)
     root.mainloop()
 
 if __name__ == '__main__':
