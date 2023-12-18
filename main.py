ccff1eff3ba5f53095d1642a107d9fcd50c3af43
WafflzPlz <michael.kogler1@gmx.at>
2023-12-18 15:36:13 +0100

    drawing pic in tkinter window and scaling

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import tkinter as tk
 import os
 from dotenv import load_dotenv
+from PIL import ImageTk, Image
 
 load_dotenv()
 PIC_PATH = os.getenv("PIC_PATH")
 
 def main():
-    master = tk.Tk()
-    label = tk.Label(master, width=128, height=128, image=os.path.join(PIC_PATH, "idle.png"))
+    root = tk.Tk()
+    root.geometry("128x128")
+
+    img = Image.open(os.path.join(PIC_PATH, "idle.png"))
+    img = img.resize((128, 128), Image.NEAREST)
+    img = ImageTk.PhotoImage(img)
+    #img = tk.PhotoImage(file=os.path.join(PIC_PATH, "idle.png"))
+
+    label = tk.Label(image=img)
     label.pack()
-    master.mainloop()
+    root.mainloop()
 
 if __name__ == '__main__':
     main()
